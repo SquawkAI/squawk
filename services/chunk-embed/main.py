04889b97556792317e6b4c50156f7cfc5e668f3a
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-07-30 00:53:23 -0500

    Update imports

--- services/chunk-embed/main.py
+++ services/chunk-embed/main.py
@@ -8,7 +8,6 @@ from supabase import create_client, Client
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, UnstructuredMarkdownLoader, UnstructuredWordDocumentLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
 
 load_dotenv()
 
