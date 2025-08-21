d8015248037b7e74932ca42aeeeec47339c3a201
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-08-21 12:45:51 -0500

    Fix auth state bug

--- web/app/projects/[id]/layout.tsx
+++ web/app/projects/[id]/layout.tsx
@@ -4,8 +4,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect /* or notFound */ } from "next/navigation";
 import { supabase } from "@/lib/supabase";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import AuthSessionProvider from "@/components/AuthSessionProvider";
 
 export default async function Layout({
     children,
@@ -34,15 +32,8 @@ export default async function Layout({
     }
 
     return (
-        <SidebarProvider>
-            <main className="flex-grow h-screen flex flex-col p-4 sm:p-6 lg:p-8 pb-0">
-                <div className="flex md:hidden mb-4">
-                    <SidebarTrigger />
-                </div>
-                <AuthSessionProvider>
-                    <div className="flex-1">{children}</div>
-                </AuthSessionProvider>
-            </main>
-        </SidebarProvider>
+       <>
+        { children }
+        </>
     );
 }
