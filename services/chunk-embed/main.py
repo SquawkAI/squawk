76c9f28ee2f6b7e49629334f8ad4880a1fecb3a4
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-09-24 09:59:15 -0500

    Remove Sentry

--- services/chunk-embed/main.py
+++ services/chunk-embed/main.py
@@ -8,9 +8,6 @@ import uuid
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request
 from supabase import create_client, Client
 
-import sentry_sdk
-from sentry_sdk.crons import monitor
-
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, UnstructuredMarkdownLoader, UnstructuredWordDocumentLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
@@ -27,49 +24,6 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 app = FastAPI()
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# Enable sentry monitoring in production
-if APP_ENV == "production":
-    SENTRY_DSN = os.getenv("SENTRY_DSN")
-    MONITOR_SLUG = os.getenv("SENTRY_MONITOR_SLUG")
-    HEARTBEAT_SEC = int(os.getenv("SENTRY_MONITOR_INTERVAL_SEC", "3600"))
-
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        # Add data like request headers and IP for users,
-        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
-        send_default_pii=True,
-    )
-
-    @app.on_event("startup")
-    async def start_heartbeat():
-        # store task so we can cancel on shutdown
-        async def _beat():
-            # send one check-in immediately at boot
-            with monitor(monitor_slug=MONITOR_SLUG):
-                pass
-            while True:
-                try:
-                    await asyncio.sleep(HEARTBEAT_SEC)
-                    with monitor(monitor_slug=MONITOR_SLUG):
-                        pass
-                except asyncio.CancelledError:
-                    # allow clean shutdown
-                    break
-                except Exception as e:
-                    # report any unexpected heartbeat failure but keep looping
-                    sentry_sdk.capture_exception(e)
-
-        app.state.sentry_heartbeat_task = asyncio.create_task(_beat())
-
-    @app.on_event("shutdown")
-    async def stop_heartbeat():
-        task = getattr(app.state, "sentry_heartbeat_task", None)
-        if task and not task.done():
-            task.cancel()
-            try:
-                await task
-            except Exception:
-                pass
 
 # TODO: Determine best embeddings
 embeddings_model = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -80,11 +34,6 @@ SUPPORTED_TYPES = {
     # ".md": UnstructuredMarkdownLoader,
     # ".docx": UnstructuredWordDocumentLoader,
 }
-
-
-@app.get("/sentry-debug")
-async def trigger_error():
-    division_by_zero = 1 / 0
 
 
 @app.get('/status')
